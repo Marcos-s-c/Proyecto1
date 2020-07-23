c1b9cdf83d48db00de5762b700131fc1b7634b0a
Marcos-s-c <66282209+Marcos-s-c@users.noreply.github.com>
2020-07-22 18:11:36 -0600

    Inicio
    creacion de la estructura del proyecto.

---
+++ api/index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../cliente")));
+
+app.listen(2020, function () {
+  console.log("Servidor corriendo en el puerto:2020");
+});
