72c2276c68fbfcfaae56b63464b9f404ede82bc2
Marcos-s-c <66282209+Marcos-s-c@users.noreply.github.com>
2020-07-22 18:34:26 -0600

    cambio

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
-var express = require("express");
-var path = require("path");
+var express = require('express');
+var path = require('path');
 var app = express();
+
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../cliente")));
 
-app.listen(2020, function () {
-  console.log("Servidor corriendo en el puerto:2020");
-});
+app.listen(2020,function () {
+    console.log("Servidor corriendo en el puerto:2020")
+})
