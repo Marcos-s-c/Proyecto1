c2e168e438eb23b61a7f0e9a31ba8cdd22e2dcfb
SilviaEle <68663130+SilviaEle@users.noreply.github.com>
2020-08-02 01:33:36 -0600

    validaciones

--- cliente/components/convenios/convenios.js
+++ cliente/components/convenios/convenios.js
@@ -1,4 +1,14 @@
-/*Función para mostrar y ocultar los inputs de agregar*/
+//Función agregar
+function add() {
+  showAndHideAdd();
+  validateEmptySpaces();
+}
+
+function edit() {
+  showAndHideEdit();
+  validateEmptySpaces();
+}
+//Función para mostrar y ocultar los inputs de agregar
 function showAndHideAdd() {
   var add = document.getElementById("add");
   var edit = document.getElementById("edit");
@@ -10,7 +20,7 @@ function showAndHideAdd() {
   }
 }
 
-/*Función para mostrar y ocultar el espacios de editar*/
+//Función para mostrar y ocultar el espacios de editar
 
 function showAndHideEdit() {
   var add = document.getElementById("add");
@@ -21,4 +31,19 @@ function showAndHideEdit() {
   } else {
     edit.style.display = "block";
   }
+}
+
+//Validar espacios
+function validateEmptySpaces() {
+  var email = document.querySelector("#email");
+  regexCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  if (email.value == "" || !regexCorreo.test(email.value)) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "El correo no ha sido registrado",
+      footer: "<a href>Why do I have this issue?</a>",
+    });
+  }
+  email.classList.add("error");
 }
