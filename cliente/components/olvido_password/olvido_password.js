17858dedbf2232056f161b240c3d08cf3339617e
SilviaEle <68663130+SilviaEle@users.noreply.github.com>
2020-08-02 19:08:27 -0600

    Formato

--- cliente/components/olvido_password/olvido_password.js
+++ cliente/components/olvido_password/olvido_password.js
@@ -1,4 +1,12 @@
+function sendEmail() {
+  var validateResult = validateEmptySpaces();
+  if (validateResult == true) {
+    window.location =
+      "/cliente/components/envio_correo_olvido_password/envio_correo_olvido_password.html";
+  }
+}
 function validateEmptySpaces() {
+  var result = false;
   var email = document.querySelector("#email");
   regexCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
   if (email.value == "" || !regexCorreo.test(email.value)) {
@@ -8,6 +16,9 @@ function validateEmptySpaces() {
       text: "El correo no ha sido registrado",
       footer: "<a href>Why do I have this issue?</a>",
     });
+    email.classList.add("error");
+  } else {
+    result = true;
   }
-  email.classList.add("error");
+  return result;
 }
