e63de3e5ff4716753d6ce15008ecdc2427f511f1
Marcos-s-c <66282209+Marcos-s-c@users.noreply.github.com>
2020-08-03 09:23:20 -0600

    cambios

--- cliente/components/agregar_empleado/agregar_empleado.js
+++ cliente/components/agregar_empleado/agregar_empleado.js
@@ -7,35 +7,33 @@ function changePassword() {
   }
 }
 
+function validar() {
+  var name = document.getElementById("name");
+  var cedula = document.getElementById("cedula");
+  var fecha_nacimiento = document.getElementById("fecha-nacimiento");
+  var numero = document.getElementById("numero");
+  var email_register = document.getElementById("email-register");
+  var puestoEmpleado = document.getElementById("puesto-empleado");
+  var fechaContratacion = document.getElementById("fecha-de-contratacion");
 
-function validar(){
-    var name = document.getElementById("name");
-    var cedula = document.getElementById("cedula");
-    var fecha_nacimiento = document.getElementById("fecha-nacimiento");
-    var numero  = document.getElementById("numero");
-    var email_register = document.getElementById("email-register");
-    var puestoEmpleado = document.getElementById("puesto-empleado");
-    var fechaContratacion = document.getElementById("fecha-de-contratacion");
-
-
-    if (
-        valiteBlanks([
-          name,
-          cedula,
-          fecha_nacimiento,
-          numero,
-          email_register,
-          puestoEmpleado,
-          fechaContratacion
-        ])
-      ) {
-       if(validateEmail(email_register.value)){
-        return window.location.href = '/cliente/components/ver_empleados/ver_empleado.html';
-       } else {
-           return false
-       }
-      } else {
-        return false;
-      }
+  if (
+    valiteBlanks([
+      name,
+      cedula,
+      fecha_nacimiento,
+      numero,
+      email_register,
+      puestoEmpleado,
+      fechaContratacion,
+    ])
+  ) {
+    if (validateEmail(email_register.value)) {
+      return (window.location.href =
+        "/cliente/components/convenios/convenios.html");
+    } else {
+      return false;
     }
-    
+  } else {
+    return false;
+  }
+}
