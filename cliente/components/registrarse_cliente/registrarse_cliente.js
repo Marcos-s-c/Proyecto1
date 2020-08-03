4f5cb4dc1b776a54d508ce1669daa45c6d1b9138
Marcos-s-c <66282209+Marcos-s-c@users.noreply.github.com>
2020-08-03 04:40:06 -0600

    cambios

--- cliente/components/registrarse_cliente/registrarse_cliente.js
+++ cliente/components/registrarse_cliente/registrarse_cliente.js
@@ -1,23 +1,20 @@
 function validar() {
   var name = document.getElementById("name");
-  var apellido = document.getElementById("apellido");
   var cedula = document.getElementById("cedula");
   var fecha_nacimiento = document.getElementById("fecha-nacimiento");
-  var numero_de_telefono = document.getElementById("numero");
+  var numero = document.getElementById("numero");
   var email_register = document.getElementById("email-register");
-
   if (
     valiteBlanks([
       name,
-      apellido,
       cedula,
       fecha_nacimiento,
-      numero_de_telefono,
-      email_register,
-    ])
-  ) {
-    return validateEmail(email_register.value);
-  } else {
-    return false;
-  }
+      numero,
+      email_register
+    ]))
+  {
+      return window.location.href = "/cliente/components/iniciar_sesion/inicio_sesion.html";
+    } else {
+      return false;
+    }
 }
