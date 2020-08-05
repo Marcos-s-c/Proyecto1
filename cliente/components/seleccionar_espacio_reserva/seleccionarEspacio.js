385e79a0eb08b6df701f4ba9300ecdd857807a86
daya-0393 <61110559+daya-0393@users.noreply.github.com>
2020-08-05 23:37:26 +0200

    new chenges on files

--- cliente/components/seleccionar_espacio_reserva/seleccionarEspacio.js
+++ cliente/components/seleccionar_espacio_reserva/seleccionarEspacio.js
@@ -1,20 +1,3 @@
-function displayDropMenu() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
  //FUNCION QUE RETORNA UN VALOR RANDOM ENTRE 1 Y 0
   function getSpotStatus(){
     return Math.round(Math.random(1,0));;
