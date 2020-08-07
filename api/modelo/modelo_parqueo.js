ec323a577aef8aef8e01d9f4faa1db87776c517b
SilviaEle <68663130+SilviaEle@users.noreply.github.com>
2020-08-06 23:01:00 -0600

    atributo tarifas módulo parqueo

--- api/modelo/modelo_parqueo.js
+++ api/modelo/modelo_parqueo.js
@@ -1,13 +1,12 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var modeloParqueo = mongoose.Schema({
-    _id:mongoose.Schema.Types.ObjectId,
-    nombreFantasia: String,
-    direccion: String,
-    ubicacion: Number,
-    campos: Array,
-   
+  _id: mongoose.Schema.Types.ObjectId,
+  nombreFantasia: String,
+  direccion: String,
+  ubicacion: Number,
+  campos: Array,
+  tarifas: Array,
 });
 
-
-module.exports = mongoose.model("Parqueo", modeloParqueo, "Parqueos" );
+module.exports = mongoose.model("Parqueo", modeloParqueo, "Parqueos");
