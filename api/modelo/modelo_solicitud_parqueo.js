6e53574e8d16a77765876a6b17010e7c1408e821
SilviaEle <68663130+SilviaEle@users.noreply.github.com>
2020-08-07 22:11:06 -0600

    Update modelo_solicitud_parqueo.js

--- api/modelo/modelo_solicitud_parqueo.js
+++ api/modelo/modelo_solicitud_parqueo.js
@@ -1,4 +1,19 @@
 "use strict";
 let mongoose = require("mongoose");
 
-let solicitudSchema = new mongoose.Schema({});
+let solicitudSchema = new mongoose.Schema({
+  nombreDelPropietario: { type: String, required: true },
+  correo: { type: String, unique: true, required: true },
+  cedula: { type: String, unique: true, required: true },
+  nombreDelParqueo: { type: String, required: true },
+  provincia: { type: String, required: true },
+  canton: { type: String, required: true },
+  distrito: { type: String, required: true },
+  nombreDelCentroComercial: { type: String, required: true },
+});
+
+module.exports = mongoose.model(
+  "Solicitud",
+  solicitudSchema,
+  "SolicitudesDeParqueo"
+);
