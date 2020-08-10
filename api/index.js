d35571127125676ef8e45e007f7f9c746e23fdc0
SilviaEle <68663130+SilviaEle@users.noreply.github.com>
2020-08-10 15:33:21 -0600

    Parqueo

--- api/index.js
+++ api/index.js
@@ -1,20 +1,14 @@
-var express = require("express");
 var path = require("path");
+var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
-<<<<<<< HEAD
 var listarUsuarios = require("./servicios/listar_usuarios");
-var guardarUsuarios = require("./servicios/guardar_usuarios");
 var logIn = require("./servicios/login");
 var guardarSolicitudes = require("./servicios/guardar_solicitudes");
-=======
-var listarUsuarios = require("./servicios/listar_usuarios")
-var guardarUsuarios = require("./servicios/guardar_usuarios")
-var logIn = require("./servicios/login")
+var guardarUsuarios = require("./servicios/guardar_usuarios");
 var logOut = require("./servicios/logout");
 const authentication = require("./middleware/authentication");
-const public_dir = express.static(path.join(__dirname, "../cliente"))
->>>>>>> 565a070a6744e5447be4f0473495896a08b32fd5
+const public_dir = express.static(path.join(__dirname, "../cliente"));
 
 //coneccion de base de datos
 mongoose
@@ -24,37 +18,24 @@ mongoose
   )
   .then((db) => {
     console.log("Base de datos conectada");
-  })
-  .catch((err) => {
-    console.log(err);
   });
 
 app.use(express.json());
-<<<<<<< HEAD
 app.use(express.static(path.join(__dirname, "../cliente")));
-=======
 app.use(public_dir);
->>>>>>> 565a070a6744e5447be4f0473495896a08b32fd5
 
 app.use(guardarUsuarios);
 app.use(listarUsuarios);
 app.use(logIn);
-<<<<<<< HEAD
 app.use(guardarSolicitudes);
-=======
 app.use(logOut);
-
->>>>>>> 565a070a6744e5447be4f0473495896a08b32fd5
 
 app.listen(4040, function () {
   console.log("Servidor corriendo en el puerto:4040");
 });
-<<<<<<< HEAD
 
 // const jwt = require('jsonwebtoken')
 
 // const token = jwt.sign({ _id: user._id.toString()}, 'proyectonuevo')
 
 //   console.log(token)
-=======
->>>>>>> 565a070a6744e5447be4f0473495896a08b32fd5
