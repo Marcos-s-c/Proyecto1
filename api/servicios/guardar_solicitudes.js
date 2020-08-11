84fab74ac3d3f8c5fdfd344a2498675744f154d9
SilviaEle <68663130+SilviaEle@users.noreply.github.com>
2020-08-10 23:37:44 -0600

    Update guardar_solicitudes.js

--- api/servicios/guardar_solicitudes.js
+++ api/servicios/guardar_solicitudes.js
@@ -4,6 +4,25 @@ var router = express.Router();
 var ParkingRequest = require("../modelo/modelo_solicitud_parqueo");
 const { request } = require("express");
 
-router.post("/solicitud_parqueo/guardar", function (req, res) {});
+router.post("/solicitud_parqueo/guardar", function (req, res) {
+  var SaveRequests = new ParkingRequest({
+    _id: new mongoose.Types.ObjectId(),
+    ownersName: req.body.ownersName,
+    usersId: req.body.usersId,
+    email: req.body.email,
+    dateOfBirth: req.body.email,
+    parkingName: req.body.email,
+    address: req.body.address,
+    shoppingCenter: req.body.shoppingCenter,
+  });
+
+  SaveRequests.save()
+    .then(function (result) {
+      res.json(result);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
 
 module.exports = router;
