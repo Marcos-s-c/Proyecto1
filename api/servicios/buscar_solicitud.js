4a358146514960318583a9f17c23a3cefa90f503
SilviaEle <68663130+SilviaEle@users.noreply.github.com>
2020-08-14 20:18:02 -0600

    search request

---
+++ api/servicios/buscar_solicitud.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+var mongoose = require("mongoose");
+
+var ParkingRequest = require("../modelo/modelo_solicitud_parqueo");
+
+router.get("/solicitud_parqueo/buscar", function (req, res) {});
+
+module.exports = router;
