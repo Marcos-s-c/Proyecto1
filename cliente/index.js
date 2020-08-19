01bcf47fa0940d2580ada9142c691758855fc1f4
SilviaEle <68663130+SilviaEle@users.noreply.github.com>
2020-08-19 11:18:47 -0600

    index

--- cliente/index.js
+++ cliente/index.js
@@ -5,47 +5,55 @@ async function logInValidation() {
     password: document.getElementById("password").value,
   };
 
-
   //Este switch es para identificar el tipo de usario que intenta ingresar y asi saber en que coleccion buscar
 
   var route = () => {
     switch (data.rol) {
-      case 'parqueo':
-        return 'parqueos/login';
-      case 'cliente':
-        return 'personas/login';
-      case 'empresa':
-        return 'empresas/login';
-      case 'admin':
-        return 'admin/login';
+      case "parqueo":
+        return "parqueos/login";
+      case "cliente":
+        return "personas/login";
+      case "empresa":
+        return "empresas/login";
+      case "admin":
+        return "admin/login";
       default:
         return "none";
     }
   };
 
   try {
-    const response = await fetch(route(), { method: 'POST', body: JSON.stringify(data), headers: { 'Content-Type': 'application/json' } })
-    if(response.status == 400){
-      alert("credenciales incorrectos")
-    }else{
+    const response = await fetch(route(), {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status == 400) {
+      alert("credenciales incorrectos");
+    } else {
       console.log(response);
-      
+
       const jsonResp = await response.json();
-      localStorage.setItem("token",  jsonResp.token)
-      document.cookie = 'token=' +  jsonResp.token
-      console.log(jsonResp);
+      localStorage.setItem("token", jsonResp.token);
+      localStorage.setItem("user", jsonResp.user);
+      document.cookie = "token=" + jsonResp.token;
+      document.cookie = "user=" + jsonResp.user;
 
       if (jsonResp.user.rol === "cliente") {
-        console.log("cliente")
-        window.location.href = './components/Buscar_Parqueo/buscar_parqueo.html';
+        console.log("cliente");
+        window.location.href =
+          "./components/Buscar_Parqueo/buscar_parqueo.html";
       } else if (jsonResp.user.rol === "empresa") {
-        console.log("empresa")
-        window.location.href = "./components/Listar_Empleados_de_Empresa/empleados.html";
+        console.log("empresa");
+        window.location.href =
+          "./components/Listar_Empleados_de_Empresa/empleados.html";
       } else if (jsonResp.user.rol === "parqueo") {
-        console.log("parqueo")
-        window.location.href = "./components/Listar_Espacios_Parqueo/listarEspacios.html";
-      } else if(jsonResp.user.rol === "admin"){
-        window.location.href = "./components/Perfl_Administrador/ver_perfil_administrador.html";
+        console.log("parqueo");
+        window.location.href =
+          "./components/Listar_Espacios_Parqueo/listarEspacios.html";
+      } else if (jsonResp.user.rol === "admin") {
+        window.location.href =
+          "./components/Perfl_Administrador/ver_perfil_administrador.html";
       }
     }
   } catch (error) {
